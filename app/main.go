015449a3e3c4776a095f53d4f5679db25b0596f8
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-06-01 20:26:52 +0530

    stage 2 done

--- app/main.go
+++ app/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	// Uncomment this block to pass the first stage!
 	// "os"
 	// "os/exec"
@@ -14,15 +16,22 @@ func main() {
 
 	// Uncomment this block to pass the first stage!
 	//
-	// command := os.Args[3]
-	// args := os.Args[4:len(os.Args)]
-	//
-	// cmd := exec.Command(command, args...)
+	command := os.Args[3]
+	args := os.Args[3:len(os.Args)]
+
+	cmd := &exec.Cmd{
+		Path:   command,
+		Args:   args,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
 	// output, err := cmd.Output()
-	// if err != nil {
-	// 	fmt.Printf("Err: %v", err)
-	// 	os.Exit(1)
-	// }
-	//
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+
 	// fmt.Println(string(output))
 }
