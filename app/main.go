c25b595fcf294a6115fd4bb8c723ef0ed6f55b7a
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-06-01 20:46:02 +0530

    stage 4 done

--- app/main.go
+++ app/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
+	"syscall"
 	// Uncomment this block to pass the first stage!
 	// "os"
 	// "os/exec"
@@ -19,6 +23,27 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[3:len(os.Args)]
 
+	chrootDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		fmt.Printf("error creating chroot dir: %v", err)
+		os.Exit(1)
+	}
+
+	if err = copyExecutableIntoDir(chrootDir, command); err != nil {
+		fmt.Printf("error copying executable into chroot: %v", err)
+		os.Exit(1)
+	}
+
+	if err = createDevNull(chrootDir); err != nil {
+		fmt.Printf("error creating /dev/null: %v", err)
+		os.Exit(1)
+	}
+
+	if err = syscall.Chroot(chrootDir); err != nil {
+		fmt.Printf("chroot error: %v", err)
+		os.Exit(1)
+	}
+
 	cmd := &exec.Cmd{
 		Path:   command,
 		Args:   args,
@@ -27,7 +52,7 @@ func main() {
 	}
 
 	// output, err := cmd.Output()
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		if e, ok := err.(*exec.ExitError); ok {
 			fmt.Println("is exec error")
@@ -38,4 +63,45 @@ func main() {
 	}
 
 	// fmt.Println(string(output))
+}
+
+func copyExecutableIntoDir(chrootDir, command string) error {
+	executablePathInChrootDir := path.Join(chrootDir, command)
+	if err := os.MkdirAll(path.Dir(executablePathInChrootDir), 0750); err != nil {
+		return err
+	}
+
+	return copyFile(command, executablePathInChrootDir)
+}
+
+func copyFile(sourceFilePath, destFilePath string) error {
+	sourceFileStat, err := os.Stat(sourceFilePath)
+	if err != nil {
+		return err
+	}
+
+	sourceFile, err := os.Open(sourceFilePath)
+	if err != nil {
+		return err
+	}
+
+	defer sourceFile.Close()
+
+	destinationFile, err := os.OpenFile(destFilePath, os.O_RDWR|os.O_CREATE, sourceFileStat.Mode())
+	if err != nil {
+		return err
+	}
+
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	return err
+}
+
+func createDevNull(chrootDir string) error {
+	if err := os.MkdirAll(path.Join(chrootDir, "dev"), 0750); err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path.Join(chrootDir, "dev", "null"), []byte{}, 0644)
 }
